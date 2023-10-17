09d1b6810e5654b86027f35d671ebb1653f3f1fa
Lukáš Vitebský <vitebluk@fit.cvut.cz>
2023-10-17 11:02:48 +0200

    PT_01: commit version 3.0
    vector instead of unordered_map
    full points 11.5/10 points on PT

--- progtest_1/progtest_1.cpp
+++ progtest_1/progtest_1.cpp
@@ -43,26 +43,16 @@ using namespace std;
 struct Tnode {
     Tnode() = default;
 
-    bool visited = 0;
-    Path parent_path = {0, 0, 0};
+    bool n_visited = 0;
     size_t distance = 0;
     size_t n_indegree = 0;
     vector<Path> n_children;
+    Path parent_path = {0, 0, 0};
 };
 
-void construct_graph(const vector<Path> &all_paths, stack<size_t> &starting_nodes, unordered_map<size_t, Tnode> &nodesData);
-void bfs(const vector<Path> &all_paths, vector<Path> &longest_path, stack<size_t> &que, unordered_map<size_t, Tnode> &nodesData);
-vector<Path> create_path(unordered_map<size_t, Tnode> &nodesData, size_t node);
-
-//~  ########## DELETE ##########
-void print_path(const vector<Path> &longest_path) {
-    // cout << endl
-    //  << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-    cout << "Longest path: " << endl;
-    for (const auto &p : longest_path) {
-        cout << p.from << " -> " << p.to << "  # " << p.length << endl;
-    }
-}
+void construct_graph(const vector<Path> &all_paths, stack<size_t> &starting_nodes, vector<Tnode> &nodesData);
+void bfs(vector<Path> &longest_path, stack<size_t> &que, vector<Tnode> &nodesData);
+vector<Path> create_path(vector<Tnode> &nodesData, size_t node);
 
 vector<Path> longest_track(size_t points, const vector<Path> &all_paths) {
     vector<Path> longest_path; // final path returned by the functions
@@ -70,58 +60,44 @@ vector<Path> longest_track(size_t points, const vector<Path> &all_paths) {
         return longest_path;
 
     stack<size_t> que; // queue of nodes
-    unordered_map<size_t, Tnode> nodesData;
-    nodesData.reserve(all_paths.size() * 1.5);
-    // unordered_map<size_t, vector<Path>> croads_children;
+    vector<Tnode> nodesData(points);
+    nodesData.reserve(points);
 
     // topologically sort the graph to get the starting nodes,
     // which will be placed into the que
     construct_graph(all_paths, que, nodesData);
-
-    bfs(all_paths, longest_path, que, nodesData);
-
-    // print_path(longest_path); //~  ########## DELETE ##########
+    bfs(longest_path, que, nodesData);
 
     return longest_path;
 }
 
-void construct_graph(const vector<Path> &all_paths, stack<size_t> &starting_nodes, unordered_map<size_t, Tnode> &nodesData) {
+void construct_graph(const vector<Path> &all_paths, stack<size_t> &starting_nodes, vector<Tnode> &nodesData) {
     for (const auto &path : all_paths) {
-        Tnode &node_from = nodesData[path.from]; // create the parent node in the map so that we can later check which nodes have indegree == 0
-        Tnode &node_to = nodesData[path.to];
+        Tnode &node_from = nodesData[(size_t)path.from]; // create the parent node in the map so that we can later check which nodes have indegree == 0
+        Tnode &node_to = nodesData[(size_t)path.to];
         node_from.n_children.emplace_back(path);
-
+        node_from.parent_path.from = path.from;
         node_to.n_indegree++; // increase the indegree value of TO crossroad
     }
 
     //? queue of crossroad_indegree "on the top" (with 0 indegrees)
-    // cout << "Indegrees of vertices" << endl; //~  ########## DELETE ##########
-    for (const auto &n : nodesData) {
-        if (n.second.n_indegree == 0)
-            starting_nodes.emplace(n.first);
-
-        // cout << c.first << ": " << c.second << endl; //~  ########## DELETE ##########
+    for (auto &n : nodesData) {
+        if (n.n_indegree == 0) {
+            size_t node_itself = n.parent_path.from;
+            n.parent_path = Path(node_itself, node_itself, 0); // Set the parent of the node to itself, since it is a starting node
+            starting_nodes.emplace(node_itself);
+        }
     }
 }
 
-void bfs(const vector<Path> &all_paths, vector<Path> &longest_path, stack<size_t> &que, unordered_map<size_t, Tnode> &nodesData) {
+void bfs(vector<Path> &longest_path, stack<size_t> &que, vector<Tnode> &nodesData) {
     size_t cur_longest_path = 0;
     size_t cur_longest_node = SIZE_MAX;
-
-    // Set the distance of all starting vertices to 0 and set their parents to INF
-    stack<size_t> temp_que = que;
-    while (!temp_que.empty()) {
-        size_t n = temp_que.top();
-        Tnode &node = nodesData[n]; // Get the front element
-        temp_que.pop();             // Remove the front element
-
-        node.parent_path = Path(n, n, 0); // Set the parent of the node to itself, since it is a starting node
-    }
 
     while (!que.empty()) {
         Tnode &node_from = nodesData[que.top()];
         que.pop();
-        node_from.visited = true;
+        node_from.n_visited = true;
 
         // If the crossroad doesn't have any children -> skip it
         if (node_from.n_children.empty())
@@ -130,11 +106,11 @@ void bfs(const vector<Path> &all_paths, vector<Path> &longest_path, stack<size_t
         // cout << "____checking children of node: " << node << "____" << endl; //~  ########## DELETE ##########
         // Check all paths poiting from this crossroad (check the crossroad's children)
         for (const auto &path : node_from.n_children) {
-            Tnode &node_to = nodesData[path.to];
-            if (node_to.visited && (node_to.distance >= (node_from.distance + path.length)))
+            Tnode &node_to = nodesData[(size_t)path.to];
+            if (node_to.n_visited && (node_to.distance >= (node_from.distance + path.length)))
                 continue;
 
-            node_to.visited = true;
+            node_to.n_visited = true;
             // If the parent for the given node was already created, erase the old one and update it
             node_to.parent_path = path;                          //? set the parent of the current node
             node_to.distance = node_from.distance + path.length; //? set the acutal distance of the current node
@@ -152,15 +128,16 @@ void bfs(const vector<Path> &all_paths, vector<Path> &longest_path, stack<size_t
     longest_path = create_path(nodesData, cur_longest_node);
 }
 
-vector<Path> create_path(unordered_map<size_t, Tnode> &nodesData, size_t node) {
+vector<Path> create_path(vector<Tnode> &nodesData, size_t node) {
     vector<Path> result;
 
     // Backtrace the path from the source node == node with the biggest distance
-    while (nodesData[node].parent_path.from != node) {
+    while ((size_t)nodesData[node].parent_path.from != node) {
         // Insert the Path to the start of the vector,
         // so that the result path will be automatically build in reverse (from the TOP node)
-        result.insert(result.begin(), nodesData[node].parent_path);
-        node = nodesData[node].parent_path.from;
+        const Path &p = nodesData[node].parent_path;
+        result.insert(result.begin(), p);
+        node = p.from;
     }
 
     return result;
@@ -179,10 +156,6 @@ inline const Test TESTS[] = {
     {13, 5, {{3, 2, 10}, {3, 0, 9}, {0, 2, 3}, {2, 4, 1}}},
     {11, 5, {{3, 2, 10}, {3, 1, 4}, {1, 2, 3}, {2, 4, 1}}},
     {16, 8, {{3, 2, 10}, {3, 1, 1}, {1, 2, 3}, {1, 4, 15}}},
-    // !!!!!!!!!!!!!!!!!! CUSTOM ASSERTS !!!!!!!!!!!!!!!!!!
-    {39, 13, {{7, 11, 9}, {10, 12, 11}, {0, 2, 4}, {2, 8, 2}, {0, 3, 9}, {9, 10, 9}, {3, 7, 10}, {1, 5, 5}, {5, 3, 6}, {5, 6, 4}, {6, 9, 10}, {3, 4, 5}, {4, 10, 7}, {10, 11, 6}, {2, 3, 5}, {8, 7, 7}}},
-    {100, 15, {{9, 10, 9}, {10, 11, 6}, {13, 14, 100}, {7, 11, 9}, {0, 3, 9}, {3, 7, 10}, {3, 4, 5}, {10, 12, 11}, {1, 5, 5}, {0, 2, 4}, {2, 8, 2}, {2, 3, 5}, {8, 7, 7}, {6, 9, 10}, {5, 3, 6}, {5, 6, 4}, {4, 10, 7}}},
-    // !!!!!!!!!!!!!!!!!! CUSTOM ASSERTS !!!!!!!!!!!!!!!!!!
 };
 
 #define CHECK(cond, ...)              \
